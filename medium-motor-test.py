e9bd563f60df4160d639720ffc6d8a82839ef825
LarsBergqvist <lars.g.bergqvist@gmail.com>
2020-03-09 13:54:51 +0100

    First example

---
+++ medium-motor-test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import MediumMotor, LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.led import Leds
+from ev3dev.ev3 import Sound
+
+ts = TouchSensor()
+leds = Leds()
+m = MediumMotor(OUTPUT_A)
+
+Sound.speak("Hello, I am a robot! Awaiting your command...")
+
+while True:
+    if ts.is_pressed:
+        leds.set_color("LEFT", "GREEN")
+        leds.set_color("RIGHT", "GREEN")
+        m.on(speed=45)
+    else:
+        leds.set_color("LEFT", "RED")
+        leds.set_color("RIGHT", "RED")
+        m.off()
